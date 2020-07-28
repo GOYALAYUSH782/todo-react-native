fb1d3e1c22ccd1c7f715c27bf309659d01f55d8c
a0g06il <ayush.goyal@walmart.com>
2020-07-28 20:10:32 +0530

    2nd code complete

---
+++ src/SingleNoteSummaryComponent.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  textProperties: {
+    fontSize: 24,
+    fontWeight: "500",
+    textAlign: "center"
+  },
+  textViewStyle: {
+    width: 150,
+    height: 150,
+    margin: 7,
+    padding: 5,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  dateProperties: {
+    fontSize: 12,
+    textAlign: "left"
+  }
+});
+
+const randomBackground = () => {
+  var red = Math.floor(Math.random() * 255) // 123
+  var green = Math.floor(Math.random() * 255) // 45
+  var blue = Math.floor(Math.random() * 255) // 43
+
+  // String Interpolation
+  // In a string -> isnert a value of some other data type
+  // ""  ''  ``
+
+  return `rgb(${red}, ${green}, ${blue})` // rgb(123, 45, 43)
+}
+
+const SingleNoteSummaryComponent = (props) => {
+  const { myNoteText } = props;
+  return (
+    <View backgroundColor={randomBackground()} style={styles.textViewStyle} >
+      <Text style={styles.dateProperties}>{myNoteText.date}</Text>
+      <Text
+        style={styles.textProperties}>
+        {myNoteText.text}
+      </Text>
+    </View>
+  );
+};
+
+export default SingleNoteSummaryComponent;
